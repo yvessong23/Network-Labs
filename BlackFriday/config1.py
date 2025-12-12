0e7a451b2e10f9ea4c622c4de0976eb1f7849443
Yves Song <ymandeng@vols.utk.edu>
2025-12-12 12:52:41 -0500

    Add Black Friday lab automation and evidence

---
+++ BlackFriday/config1.py
@@ -0,0 +1,108 @@
+# This is a configuration file that connects to the Cisco CML server
+# and into the lab devices
+# 1. IMPORT
+
+import os # OS for file and directory creation. This is how I will be saving my created files 
+from datetime import datetime  # Datetime library allows me to log each configuration file based on time created, or modified
+from netmiko import ConnectHandler, redispatch # netmiko allows me to connect to external devices
+from configConstants.device_config import devices, CML_SERVER # Hidden in a land far far away.. rests the information to access 
+import time
+# the server and devices (for security of course)
+
+# 2. CREATE CONSTANTS
+# At this stage it's its important to create and set the time immediately at the top of the file.
+# This is what will be used to mark the time of the configurations within the files you create and
+# any update that follows after
+
+current_time = datetime.now()
+formatted_time = current_time.strftime("%Y/%m/%d %H:%M:%S")
+print(formatted_time)
+
+
+# 3. CREATE FUNCTIONS FOR CONNECTION ACCESS
+# At this point, I could do everything in a try-catch block, but let's
+# be a little more organized and modular 
+# The first function will allow me to connect to the remote CML server
+
+def connect_to_server():
+    try:  
+        print(f"Attempting to connect to {CML_SERVER['host']}..")
+        conn = ConnectHandler(**CML_SERVER,session_log="cml_log.txt")     # connect to the CML server via the handler we imported
+        print(f"Connection to {CML_SERVER['host']} succeeded!..")
+        return conn
+    except Exception as err:    # Important to 
+        print(f"Failed to connect to {CML_SERVER['host']}..")
+        return None
+
+# The second function runs BECAUSE I'm now connected to the server,
+# Since I know the format, I begin with parsing
+# This is where I will be sending commands to the actual CML server
+
+def command_to_server(cml_connect):    
+    # View Device list, 
+    # It's important to do this as I may be sending commands to a device thats doesnt exist
+    try:
+        print("Sending command to CML server.. List")
+        response = cml_connect.send_command("list")     
+        lines = response.split('\n') 
+    
+        for index in range(len(lines)): # Run through the lines and search if the device I want exists!
+            current_line = lines[index]
+            if "Black Friday Scenario" not in current_line: # Skip irrelevant lines and headers
+                continue;
+            print("Found Black Friday Scenario")
+            
+            #if position_idx >= 0:
+            for device in devices:
+                node_name = device["node"]
+                if node_name in current_line:
+                    print("Found CoreSW1..")
+                    print("Opening CoreSW1...")
+                    cml_connect.send_command_timing(f"open /Black Friday Scenario/{node_name}/0", read_timeout=600) # Once the device exists
+                    print(f"Connected to {node_name}")
+                    return node_name 
+    except Exception as err:
+        print(f"Failed to write prompt to {CML_SERVER['host']}..")
+        return None
+
+    print("Could not find {node_name}")
+    return False
+
+def get_running_config(cml_connect):
+    cml_connect.write_channel("\r")
+    cml_connect.write_channel("en")
+    redispatch(cml_connect, device_type="cisco_ios", session_prep=False) # Redispatching allows us to change Netmiko class to Cisco CLI
+    cml_connect.send_command("terminal length 0")
+    output = cml_connect.send_command("show running-config", expect_string=r"[>#]") 
+    return output
+
+def save_config(output, node):
+    # check if filePath exists, find filePath for device
+    network_path = f"network_backups/{node}"
+    #for root, dirs, files in os.walk("."):
+    #for direct in dirs:
+    if os.path.isdir(network_path):
+        print(f"The directory {network_path} exists!")
+        working_dir = os.chdir(network_path) # Change directory
+        print(f"Current directory {os.getcwd()}")
+        file_path = formatted-time
+        if os.path.exists(path):
+            print(f"{file_path} exists!")
+        else:
+            with open(file_path, 'w') as fp:
+                fp.write()
+
+    else: 
+        print(f"The directory {network_path} does not exist")
+    #return output
+    # create file based on date
+    # write to disk
+
+
+if  __name__ == "__main__":
+    conn = connect_to_server()
+    if conn is not None:
+        node = command_to_server(conn)
+       # time.sleep(2)
+        output = get_running_config(conn)
+        save_config(output, node)
---
+++ BlackFriday/configConstants/device_config.py
@@ -0,0 +1,21 @@
+# CML CONSTANTS FOR SERVER
+import os
+
+CML_SERVER = {
+    "host":"10.0.0.53",
+    "username":"sysadmin1",
+    "password":"Mandyaves237$!",
+    "device_type":"terminal_server"
+}
+
+devices = [
+    {
+        "node":"CoreSW1",
+        "console_path": "Black Friday Scenario/CoreSW1/0",    
+        "host":"10.99.0.2",
+        "access_method":":ssh"
+    }
+]
+
+
+
