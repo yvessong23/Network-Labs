acd0e37973d423c3998ab5f5e5275a475742c64f
Yves Song <ymandeng@vols.utk.edu>
2026-09-10 20:19:38 -0400

    V1 of port scanner

---
+++ port_raw.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main(){
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    for (int i = 1024; i < 2070; i++){
+        int fd = socket(AF_INET, SOCK_RAW, );
+        if (fd < 0){
+            perror("Could not open socket!\n");
+            exit(1);
+        }
+
+        addr.sin_port = htons(i);
+        if(connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == 0){
+            printf("Connected to port %d\n", i);
+            close(fd);
+        } else {
+            close(fd);
+        }
+    }
+}
