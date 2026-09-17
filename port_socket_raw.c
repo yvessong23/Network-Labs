2f3565ef7359679c055bf54ee7b6b4175a538538
yvessong23 <yvessongm@gmail.com>
2026-09-17 19:23:14 -0400

    Updated 9/17/26

--- port_socket_raw.c
+++ port_socket_raw.c
@@ -6,7 +6,7 @@
 
 typedef struct iphdr {
     // These two share the first byte (8 bits total)
-    unsigned int version:4;  // Must be 4 for IPv4
+    unsigned int version:4;  // Must be 4 for IPv4, for Routers
     unsigned int ihl:4;      // Internet Header Length (usually 5)
 
     uint8_t  tos;            // Type of Service, aka Differentiated Services Code Point 
@@ -14,13 +14,13 @@ typedef struct iphdr {
     uint16_t id;             // Identification
     uint16_t frag_off;       // Fragment Offset & Flags
     uint8_t  ttl;            // Time to Live
-    uint8_t  protocol;       // Protocol (e.g., 6 for TCP, 17 for UDP)
+    uint8_t  protocol;       // Protocol (e.g., 6 for TCP, 17 for UDP) for OS
     uint16_t check;          // Header Checksum
     uint32_t saddr;          // Source IP Address (32-bit unsigned int)
     uint32_t daddr;          // Destination IP Address (32-bit unsigned int)
 } iphdr;
 
-void create_iphdr(int id_count){
+void create_iphdr(int id_count){ 
     iphdr *h = malloc(sizeof(iphdr));
     h->version = 4;
     h->ihl = 5;
@@ -28,22 +28,26 @@ void create_iphdr(int id_count){
     h->tot_len = 40;
     h->id = id_count;
     h->frag_off = 0;
-    h->ttl = ;
+    h->ttl = 64;
+    h->protocol = 6;
+    h->check = 0;
+    h->saddr = 0;
+    h->daddr = 0;
 }
 
 
 int main(){
 	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	//addr.sin_family = AF_INET;
+	//addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	for (int i = 1024; i < 2070; i++){
-		int fd = socket(AF_INET, SOCK_RAW, 0);
+		int fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
 		if (fd < 0){
 			perror("Could not open socket!\n");
 			exit(1);
 	    }
 	
-        addr.sin_port = htons(i);
+    //    addr.sin_port = htons(i);
         if(connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == 0){
             printf("Connected to port %d\n", i);
             close(fd);
